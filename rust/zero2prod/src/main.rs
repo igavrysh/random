45815fef8b61934dbb8be326c56efba588dc12cd
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-08-20 16:03:44 -0700

    first commit

---
+++ rust/zero2prod/src/main.rs
@@ -0,0 +1,23 @@
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+
+
+async fn health_check(req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok()
+}
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("[::1]:8000")?
+    .run()
+    .await
+}
