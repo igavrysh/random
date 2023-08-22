ef8d8dbf52cadd3106cbb0e94778ed66d3e53584
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-08-22 03:41:20 -0700

    test commit

--- rust/zero2prod/src/routes/subscriptions.rs
+++ rust/zero2prod/src/routes/subscriptions.rs
@@ -1,5 +1,6 @@
 use actix_web::{HttpResponse, web};
 use chrono::Utc;
+use tracing::Instrument;
 use uuid::Uuid;
 use sqlx::PgPool;
 
@@ -10,12 +11,54 @@ pub struct FormData {
     name: String
 }
 
+
+#[allow(clippy::async_yields_async)]
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    )
+)]
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    match insert_subscriber(&pool, &form).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
+)]
+pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
+    INSERT INTO subscriptions (id, email, name, subscribed_at)
+    VALUES ($1, $2, $3, $4)
+            "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+    Ok(())
+}
+
+/* 
+
 pub async fn subscribe(
     form: web::Form<FormData>,
     db_pool: web::Data<PgPool>,
 ) -> HttpResponse {
     let request_id = Uuid::new_v4();
-
     let request_span = tracing::info_span!(
         "Adding a new subscriber.",
         %request_id,
@@ -24,12 +67,9 @@ pub async fn subscribe(
     );
     let _request_span_guard = request_span.enter();
     
-    tracing::info!("request_id {} - Adding '{}' '{}' as a new subscriber",
-        request_id,
-        form.email,
-        form.name
+    let query_span = tracing::info_span!(
+        "Saving new subscitber details in the database"
     );
-    tracing::info!("request_id {} - Saving new subscrier details in the database", request_id);
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -41,6 +81,7 @@ pub async fn subscribe(
         Utc::now()
     )
     .execute(db_pool.get_ref())
+    .instrument(query_span)
     .await {
         Ok(_) => {
             tracing::info!("request_id {} - New subscriber details have been saved", request_id);
@@ -52,3 +93,5 @@ pub async fn subscribe(
         }
     }
 }
+
+*/
--- rust/zero2prod/src/startup.rs
+++ rust/zero2prod/src/startup.rs
@@ -1,9 +1,8 @@
-use std::net::TcpListener;
-
-use actix_web::{dev::Server, HttpServer, web, App, middleware::Logger};
-use sqlx::PgPool;
-
 use crate::routes::{subscribe, health_check};
+use actix_web::{dev::Server, HttpServer, web, App};
+use sqlx::PgPool;
+use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(
     listener: TcpListener, 
@@ -12,13 +11,12 @@ pub fn run(
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
-
     Ok(server)
 }
