b5cfc8d8ded7f8e441b7d5798bfca57faec0acd0
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-08-22 01:49:26 -0700

    add logging

--- rust/zero2prod/src/routes/subscriptions.rs
+++ rust/zero2prod/src/routes/subscriptions.rs
@@ -14,6 +14,13 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     db_pool: web::Data<PgPool>,
 ) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    log::info!("request_id {} - Adding '{}' '{}' as a new subscriber",
+        request_id,
+        form.email,
+        form.name
+    );
+    log::info!("request_id {} - Saving new subscrier details in the database", request_id);
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -26,9 +33,12 @@ pub async fn subscribe(
     )
     .execute(db_pool.get_ref())
     .await {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!("request_id {} - New subscriber details have been saved", request_id);
+            HttpResponse::Ok().finish()
+        },
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            log::error!("request_id {} - Failed to execute query: {:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
--- rust/zero2prod/src/startup.rs
+++ rust/zero2prod/src/startup.rs
@@ -1,6 +1,6 @@
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, HttpServer, web, App};
+use actix_web::{dev::Server, HttpServer, web, App, middleware::Logger};
 use sqlx::PgPool;
 
 use crate::routes::{subscribe, health_check};
@@ -12,6 +12,7 @@ pub fn run(
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
