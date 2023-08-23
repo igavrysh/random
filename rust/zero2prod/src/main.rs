cb0da296da93b04be75bd1820c4b61be7acc68dc
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-08-23 11:39:53 -0700

    optimizing docker image

--- rust/zero2prod/src/main.rs
+++ rust/zero2prod/src/main.rs
@@ -1,6 +1,6 @@
 use std::net::TcpListener;
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::{PgPool, postgres::PgPoolOptions};
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
@@ -27,9 +27,10 @@ async fn main() -> std::io::Result<()> {
     let configuration = get_configuration()
         .expect("Failed to read configuration");
 
-    let connection_pool
-        = PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
-            .expect("Failed to connect to Postgres.");
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
+        .expect("Failed to connect to Postgres.");
 
     let address = format!("{}:{}", 
         configuration.application.host, configuration.application.port
