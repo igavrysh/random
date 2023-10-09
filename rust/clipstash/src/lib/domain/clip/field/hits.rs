269b68262e305500c0c75efb77628f21d013c8de
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-10-09 08:17:51 -0700

    impl main data struct s

--- rust/clipstash/src/lib/domain/clip/field/hits.rs
+++ rust/clipstash/src/lib/domain/clip/field/hits.rs
@@ -4,8 +4,9 @@ use derive_more::Constructor;
 #[derive(Clone, Constructor, Debug, Deserialize, Serialize)]
 pub struct Hits(u64);
 
+
 impl Hits {
-    pub fn into_inner() {
-        
+    pub fn into_inner(self) -> u64 {
+        self.0
     }
 }
--- rust/clipstash/src/lib/domain/mod.rs
+++ rust/clipstash/src/lib/domain/mod.rs
@@ -1,1 +1,3 @@
 pub mod clip;
+
+pub mod time;
