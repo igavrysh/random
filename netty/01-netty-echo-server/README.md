239fcd4d4318a7077714b8860c51b45f36ec8406
Gene <zhenya.ga@gmail.com>
2026-01-25 10:59:46 -0800

    first netty example

---
+++ netty/01-netty-echo-server/README.md
@@ -0,0 +1,20 @@
+to run maven main class
+```
+mvn exec:java -Dexec.mainClass="com.mycompany.app.App"
+```
+
+to plain run java, from target folder:
+```
+java -cp my-app-1.0-SNAPSHOT.jar com.mycompany.app.App
+```
+
+
+```
+dot_clean -mn /Volumes/Samsung\ USB/test/netty-echo-server/my-app
+```
+
+
+to send to server
+```
+echo "hello ievgen" | nc 127.0.0.1 9090
+```
---
+++ netty/01-netty-echo-server/src/main/java/com/mycompany/app/EchoServer.java
@@ -0,0 +1,46 @@
+package com.mycompany.app;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
+public class EchoServer {
+    private final int port;
+
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            System.out.println("Usage: " + EchoServer.class.getName() + " <port>");
+        }
+        int port = Integer.parseInt(args[0]);
+        new EchoServer(port).start();
+    }
+
+    public void start() throws Exception {
+        final EchoServerHandler serverHandler = new EchoServerHandler();
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(group)
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(port))
+                    .childHandler(new ChannelInitializer<>() {
+                        @Override
+                        protected void initChannel(Channel ch) throws Exception {
+                            ch.pipeline().addLast(serverHandler);
+                        }
+                    });
+            ChannelFuture f = b.bind().sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+
+    }
+}
